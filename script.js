bc6a7acc888adc323e5bf79cb6f7cf2945925d97
Lydia Williams <78613129+lydnicwil@users.noreply.github.com>
2022-06-02 10:40:50 -0400

    added js variables

---
+++ script.js
@@ -0,0 +1,22 @@
+var timer = document.querySelector("#timer");
+var startGameButton = document.querySelector("#startGame");
+var viewHighscore = document.querySelector("#viewHighscore");
+
+var theQuestion = document.querySelector("#question");
+var answerOne = document.querySelector("#choiceOne");
+var answerTwo = document.querySelector("#choiceTwo");
+var answerThree = document.querySelector("#choiceThree");
+var answerFour = document.querySelector("#choiceFour");
+var correctness = document.querySelector("#correct");
+
+var showStart = document.querySelector("#startSection");
+var showQuestions = document.querySelector("#questionSection");
+var showForm = document.querySelector("#formSection");
+var showHighscore = document.querySelector("#highscoreSection");
+
+var highscoreForm = document.querySelector("#highscore");
+var highscoreName = document.querySelector("#highscoreName");
+var highscoreList = document.querySelector("#highscoreList");
+
+var reset = document.querySelector("#resetButton");
+var restart = document.querySelector("#restartButton");
